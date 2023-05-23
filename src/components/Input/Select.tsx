a5d245ed22e2ed00581f3ee9b713ae73fa6e6ae6
unogjoe <nketageorgejoe@gmail.com>
2023-05-23 16:55:10 +0100

    update select dropdown

--- src/components/Input/Select.tsx
+++ src/components/Input/Select.tsx
@@ -1,33 +1,84 @@
+// import { useState } from "react";
+// import "./Input.scss";
+
+// type InputProps = {
+//   label: string;
+//   children: React.ReactNode;
+//   style?: any;
+//   onChange?: any;
+//   onChangeCapture?: any;
+// };
+
+// const Select = (props: InputProps) => {
+//   const [labelStyle, setLabelStyle] = useState("#484646");
+//   const { label, children, style, onChange, onChangeCapture } = props;
+
+//   return (
+//     <div className="form-group" style={style}>
+//       <label style={{ color: labelStyle }} className="capitalize">
+//         {label}
+//       </label>
+//       <div className="input-wrapper select">
+//         <select
+//           onFocus={() => setLabelStyle("#2bd9c7")}
+//           onBlur={() => setLabelStyle("#484646")}
+//           onChange={onChange}
+//           onChangeCapture={onChangeCapture}
+//         >
+//           {children}
+//         </select>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Select;
+
 import { useState } from "react";
-import "./Input.scss";
+import "./select.scss";
 
-type InputProps = {
-  label: string;
-  children: React.ReactNode;
-  style?: any;
-  onChange?: any;
-  onChangeCapture?: any;
-};
-
-const Select = (props: InputProps) => {
+const Select = ({ options, onOptionSelected, label }: any) => {
+  const [toggleDropdown, setToggleDropdown] = useState(false);
   const [labelStyle, setLabelStyle] = useState("#484646");
-  const { label, children, style, onChange, onChangeCapture } = props;
+  const [selectedOption, setSelectedOption] = useState("Select an option");
+
+  const handleDropdownClicked = () => {
+    setToggleDropdown((prev) => !prev);
+  };
+
+  const handleDropdownClosed = () => {
+    setToggleDropdown(false);
+  };
+
+  const handleOptionClicked = (option: string) => {
+    onOptionSelected(option);
+    setSelectedOption(option);
+  };
 
   return (
-    <div className="form-group" style={style}>
-      <label style={{ color: labelStyle }} className="capitalize">
-        {label}
-      </label>
-      <div className="input-wrapper select">
-        <select
-          onFocus={() => setLabelStyle("#2bd9c7")}
-          onBlur={() => setLabelStyle("#484646")}
-          onChange={onChange}
-          onChangeCapture={onChangeCapture}
-        >
-          {children}
-        </select>
+    <div
+      className="dropdown"
+      tabIndex={0}
+      onBlur={handleDropdownClosed}
+      onClick={handleDropdownClicked}
+      onFocus={() => setLabelStyle("#2bd9c7")}
+    >
+      <div className="select">
+        <span  >{label}</span>
+        {/* Style for span above style={{ color: labelStyle }} */}
+        <label>{selectedOption}</label>
       </div>
+      {toggleDropdown && options?.length ? (
+        <ul className="dropdown-list" >
+          <div className="dotted">
+            {options?.map((option: string, i: number) => (
+              <li key={i} onClick={() => handleOptionClicked(option)}>
+                {option}
+              </li>
+            ))}
+          </div>
+        </ul>
+      ) : null}
     </div>
   );
 };
